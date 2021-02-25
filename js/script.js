63a24adef1ed43ee75e16ae31dda5e73a6e0c076
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-02-25 15:45:16 +0100

    Modificato

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var nome = prompt("Qual è il tuo nome?");
-var cognome = prompt("Qual è il tuo cognome?")
-var colore = prompt("Qual è il tuo colore preferito?")
+var cognome = prompt("Qual è il tuo cognome?");
+var colore = prompt("Qual è il tuo colore preferito?");
 
-document.getElementById('password').innerHTML = nome + cognome + colore + 27
+document.getElementById('password').innerHTML = nome + cognome + colore + 27;
