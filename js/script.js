83ee6bdc5e0355b9223e7041b886aaf0fa7466ba
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-02-25 14:45:43 +0100

    Modificato prompt in domanda

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-var nome = prompt("Inserisci il tuo nome");
-var cognome = prompt("Inserisci il tuo cognome")
-var colore = prompt("Inserisci il tuo colore preferito")
-console.log(nome, cognome, colore);
+var nome = prompt("Qual è il tuo nome?");
+var cognome = prompt("Qual è il tuo cognome?")
+var colore = prompt("Qual è il tuo colore preferito?")
 
 document.getElementById('password').innerHTML = nome + cognome + colore + 27
